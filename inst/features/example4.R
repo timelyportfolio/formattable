1bb46ef15fbe9075e12f582ea02b87895018bd05
timelyportfolio <kent.russell@timelyportfolio.com>
2016-05-20 10:21:20 -0500

    mostly satisfy regression table reqs

--- inst/features/example4.R
+++ inst/features/example4.R
@@ -1,6 +1,18 @@
 # from @renkun-ken html-table branch
 #  https://github.com/renkun-ken/formattable/blob/html-table/R/html_table.R
 
+
+bootstrap_dependency <- function(){
+  htmltools::htmlDependency(
+    name = "bootstrap",
+    version = "3.3.6",
+    src = c(href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/"),
+    script = "js/bootstrap.min.js",
+    stylesheet = "css/bootstrap.min.css"
+  )
+}
+
+
 #  added colhead style
 html_table <- function(
   x, align = "left", caption = NULL,
@@ -25,7 +37,7 @@ html_table <- function(
     tag("thead", list(
       tag("tr", c(class = "header",
                   .mapply(function(column, align, style) {
-                    tag("th", list(align = align, style = style, HTML(column)))
+                    tag("th", list(align = align, style = style, style = paste0("text-align:", align) , HTML(column)))
                   }, list(colnames(x), align, colhead_style), NULL)))
     )),
     tag("tbody", .mapply(function(...) {
@@ -75,10 +87,14 @@ estimateFormatter <- formatter(
 )
 
 # Add tiles to t-statistics.
-tFormatter <- formatter("span",
-                        style = x ~ style(display = "block",
-                                          padding = "0 4px", `border-radius` = "4px", `background-color` = csscolor(gradient(abs(x),"white", "orange"))),
-                        ~ fixedDigits(t, 2)
+tFormatter <- formatter(
+  "span",
+  style = x ~ style(
+    display = "block",
+    padding = "0 4px", `border-radius` = "4px",
+    `background-color` = csscolor(gradient(abs(x),"white", "orange"))
+  ),
+  ~ fixedDigits(t, 2)
 )
 
 # Example
@@ -86,16 +102,53 @@ coef.matrix <- summary(lm(Sepal.Length ~ Species * Sepal.Width, iris))$coef
 coef.df <- as.data.frame(coef.matrix)
 rownames(coef.df)[2] = "A label with some spaces"
 colnames(coef.df)[2:4] <- c("Standard<br>Error", "t", "p")
-ft <- formattable(coef.df,
-            list(Estimate = estimateFormatter, "p" = pFormatter, "t" = tFormatter))
+ft <- formattable(
+  coef.df,
+  list(
+    Estimate = estimateFormatter,
+    "Standard<br>Error" = x~digits(x,2),
+    "p" = pFormatter,
+    "t" = tFormatter
+  )
+)
 ft
 
 
 
 # play with the html_table function
+#  without bare html and not bootstrap
 library(htmltools)
 browsable(
   html_table(
-    as.htmldf(ft)
+    as.htmldf(ft),
+    include_rownames=TRUE,
+    colhead_style=paste0("vertical-align:bottom;",c("","","","font-style:italic;","font-style:italic;")),
+    align=c("left",rep("right",4))
+  )
+)
+
+browsable(
+  attachDependencies(
+    html_table(
+      as.htmldf(ft),
+      table_class="table-condensed",
+      include_rownames=TRUE,
+      colhead_style=paste0("vertical-align:bottom;",c("","","","font-style:italic;","font-style:italic;")),
+      align=c("left",rep("right",4)),
+      caption=tagList(
+        tags$caption(
+          style="caption-side:top;",
+          tags$h3("Specially Formatted Table")
+        ),
+        tags$caption(
+          style="caption-side:bottom;font-style:italic;font-size:80%;",
+          "this footer specifically designed to communicate important information"
+        )
+      )
+    ),
+    list(
+      rmarkdown:::html_dependency_jquery(),
+      bootstrap_dependency()
+    )
   )
 )
